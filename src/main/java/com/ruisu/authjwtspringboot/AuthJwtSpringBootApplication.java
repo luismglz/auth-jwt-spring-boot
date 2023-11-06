84c60a6cc9c2772e51a076ee333d7f0e2e2509b4
Luis GL <lmgl887@gmail.com>
2023-11-06 17:20:47 -0600

    add: empty project

---
+++ src/main/java/com/ruisu/authjwtspringboot/AuthJwtSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.ruisu.authjwtspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthJwtSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthJwtSpringBootApplication.class, args);
+	}
+
+}
