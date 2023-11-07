0293f079c5289e2da976f2eeeaf76e4a31cc6137
Luis GL <lmgl887@gmail.com>
2023-11-07 13:59:48 -0600

    add: rest exception handler

---
+++ src/main/java/com/ruisu/authjwtspringboot/config/RestExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.ruisu.authjwtspringboot.config;
+
+import com.ruisu.authjwtspringboot.exceptions.AppException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class RestExceptionHandler {
+    
+    @ExceptionHandler(value = {AppException.class})
+    @ResponseBody
+    public ResponseEntity<ErrorDto> handleException(AppException ex){
+        return ResponseEntity
+                .status(ex.getHttpStatus())
+                .body(new ErrorDto(ex.getMessage()));
+    }
+}
