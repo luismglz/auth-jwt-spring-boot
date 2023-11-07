240a1d86a6eb0937fd9c592c844e043984741a8d
Luis GL <lmgl887@gmail.com>
2023-11-07 14:22:33 -0600

    add: security filter chain

--- src/main/java/com/ruisu/authjwtspringboot/mappers/UserMapper.java
+++ src/main/java/com/ruisu/authjwtspringboot/mappers/UserMapper.java
@@ -3,9 +3,7 @@ package com.ruisu.authjwtspringboot.mappers;
 import com.ruisu.authjwtspringboot.dtos.UserDto;
 import com.ruisu.authjwtspringboot.entities.User;
 import org.mapstruct.Mapper;
-
 @Mapper(componentModel="spring")
 public interface UserMapper {
-
     UserDto toUserDto(User user);
 }
