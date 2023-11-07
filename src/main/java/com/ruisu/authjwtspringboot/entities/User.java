b658fe5e7e602a87a98a6e01348454d203a6eaec
Luis GL <lmgl887@gmail.com>
2023-11-07 12:47:25 -0600

    add: user entity

---
+++ src/main/java/com/ruisu/authjwtspringboot/entities/User.java
@@ -0,0 +1,32 @@
+package com.ruisu.authjwtspringboot.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "user_name", nullable = false)
+    private String userName;
+
+    @Column(name="password", nullable = false)
+    private String password;
+}
